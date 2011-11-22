29220b3033721ba3bf22280d70f8178ba7e31adc
franzese <thechosenrick@gmail.com>
2011-11-21 23:45:28 -0500

    Legs and body logic working

--- factory.py
+++ factory.py
@@ -8,41 +8,38 @@ x = 0
 y = 1
 size = (32, 32)
 
+
+
 """ init """
 robot = Image.new('RGB', size, 'white')
 draw = ImageDraw.Draw(robot)
 alpha = Image.new('L', size, 0)
 
-
-
 """ legs """
-border = random.randint(5, 15)
+border = random.randint(5, 14)
 height = random.randint(3, 8)
 
-bot_left = (0 + border, robot.size[y] - 1)
-top_left = (0 + border, robot.size[y] - height)
+print "Leg Height: " + str(height) + "\tLeg Border: " + str(border) + "\tLeg Width: " + str(size[x] - (border * 2))
+
+bot_left = (border, robot.size[y] - 1)
+top_left = (border, robot.size[y] - height)
 bot_right = (robot.size[x] - 1 - border, robot.size[y] - 1)
 top_right = (robot.size[x] - 1 - border, robot.size[y] - height)
-
-#print "Line 1\nBL: " + str(bot_left) + "\nTL: " + str(top_left)
-#print "Line 2\nBR: " + str(bot_right) + "\nTR: " + str(top_right)
 
 draw.line((bot_left, top_left), fill=colors[0])
 draw.line((bot_right, top_right), fill=colors[0])
 
 
 """ body """
-border = random.randint() # body must be > leg width + 1px and must leave >3px for arms
+border = random.randint(4, border - 2) # body must be > leg width + 1px and must leave >3px for arms
 height = random.randint(3, 8)
 
-print "Body Offset: " + str(border) + "\nBody Height: " + str(height)
+print "Body Height: " + str(height) + "\tBody Border: " + str(border)
 
-bot_left = (robot.size[1]/2 - border, top_left[0])
-bot_right = (robot.size[1]/2 + border, top_right[0])
-top_left = (robot.size[1]/2 - border, bot_left[1] + height)
-top_right = (robot.size[1]/2 + border, bot_right[1] + height)
+bot_left = (border,top_left[y])
+top_right = (robot.size[x] - 1 - border, top_right[y] - height)
 
-#print "\nBL: " + str(bot_left) + "\nTR: " + str(top_right)
+#print "BL: " + str(bot_left) + "\tTR: " + str(top_right)
 draw.rectangle([bot_left, top_right], fill=colors[2], outline=colors[0])
 
 
@@ -51,4 +48,3 @@ draw.rectangle([bot_left, top_right], fill=colors[2], outline=colors[0])
 del draw
 robot.show()
 robot.save('robot.png')
-print '"robot.png" saved.'
