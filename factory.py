fc5f982dd7115f17ac42c1def8f83463034e5b88
franzese <thechosenrick@gmail.com>
2011-11-26 14:55:23 -0500

    New factory method

--- factory.py
+++ factory.py
@@ -10,77 +10,83 @@ y = 1
 size = (32, 32)
 
 
+class robo_piece:
+    width = 0
+    height = 0
+    
+    def __str__(self):
+        return str(self.width) + "px, " + str(self.height) + "px"
+
 class robot:
+    head = robo_piece()
+    arms = robo_piece()
+    body = robo_piece()
+    legs = robo_piece()
+    
     def __init__(self, size):
-        # maximum height is 25% of the canvas
-        maxheight = int(math.floor(size[y]/4))
-        
-        self.head.width = random.randit(8, size[x])
-        self.head.height = random.randit(7, maxheight)
+        # maximum height is 33% of the canvas
+        maxheight = int(math.ceil(size[y]/3.0))
         
         self.arms.width = random.randint(2,3)
         self.arms.height = 1
         
-        self.body.width = random.randint(4, size[x] - self.arms.width)
-        self.legs.height = random.randint(4, maxheight)
+        self.body.width = random.randrange(4, size[x] - self.arms.width + 1, 2)
+        self.body.height = random.randint(4, maxheight)
         
-        self.legs.width = random.randint(4, self.body.width)
+        self.legs.width = random.randrange(4, self.body.width + 1, 2)
         self.legs.height = random.randint(3, maxheight)
+        
+        self.head.width = random.randrange(8, size[x] + 1, 2)
+        self.head.height = random.randint(7, maxheight)
+        
+    def __str__(self):
+        return 'head: ' + str(self.head) + '\nbody: ' + str(self.body) + '\narms: ' + str(self.arms) + '\nlegs: ' + str(self.legs)
 
-
-def factory1 (colors, size):
-    robot = Image.new('RGB', size, 'rgb(220,220,220)')
-    draw = ImageDraw.Draw(robot)
-    alpha = Image.new('L', size, 0)
-
-
-
-def factory2 (colors, size):
+def factory(colors, size):
     """ init """
-    robot = Image.new('RGB', size, 'rgb(220,220,220)')
-    draw = ImageDraw.Draw(robot)
+    bot = robot(size)
+    canvas = Image.new('RGB', size, 'rgb(220,220,220)')
+    draw = ImageDraw.Draw(canvas)
     alpha = Image.new('L', size, 0)
 
     """ legs """
-    border = random.randint(5, 14)
-    height = random.randint(3, 8)
-
-    print "Leg Height: " + str(height) + "\tLeg Border: " + str(border) + "\tLeg Width: " + str(size[x] - (border * 2))
-
-    bot_left = (border, robot.size[y] - 1)
-    top_left = (border, robot.size[y] - height)
-    bot_right = (robot.size[x] - 1 - border, robot.size[y] - 1)
-    top_right = (robot.size[x] - 1 - border, robot.size[y] - height)
-
-    draw.line((bot_left, top_left), fill=colors[0])
-    draw.line((bot_right, top_right), fill=colors[0])
-
+    leg_offset = int(round((size[x] - bot.legs.width)/2))
+    leg_left = ((leg_offset, size[y]), (leg_offset, size[y] - bot.legs.height))
+    leg_right = ((leg_offset + bot.legs.width - 1, size[y]), (leg_offset + bot.legs.width - 1, size[y] - bot.legs.height))
+    draw.line(leg_left, fill=colors[0])
+    draw.line(leg_right, fill=colors[0])
 
     """ body """
-    border = random.randint(4, border) # body must be > leg width + 1px and must leave >3px for arms
-    height = random.randint(3, 8)
-
-    print "Body Height: " + str(height) + "\tBody Border: " + str(border)
-
-    bot_left = (border,top_left[y])
-    #top_left = (border,)
-    top_right = (robot.size[x] - 1 - border, top_right[y] - height)
-
-    #print "BL: " + str(bot_left) + "\tTR: " + str(top_right)
+    body_offset = int(round((size[x] - bot.body.width)/2))
+    body_left = body_offset
+    body_right = body_offset + bot.body.width - 1
+    body_bottom = size[y] - 1 - bot.legs.height
+    body_top = size[y] - bot.legs.height - bot.body.height
+    bot_left = (body_offset, size[y] - 1 - bot.legs.height)
+    top_right = (body_offset + bot.body.width - 1, size[y] - bot.legs.height - bot.body.height)
     draw.rectangle([bot_left, top_right], fill=colors[2], outline=colors[0])
 
+    """ arms """
+    arm_height = size[y] - bot.legs.height - bot.body.height + int(math.ceil(bot.body.height/2))
+    arm_offset = (size[x] - bot.body.width - (bot.arms.width * 2))/2
+    arm_left = ((arm_offset ,arm_height),(arm_offset + bot.arms.width - 1, arm_height))
+    arm_right =  ((body_offset + bot.body.width, arm_height), (body_offset + bot.body.width + bot.arms.width - 1, arm_height))
+    draw.line(arm_left, fill=colors[0])
+    draw.line(arm_right, fill=colors[0])
+
     """ head """
+    offset = int(round((size[x] - bot.head.width)/2))
+    bot_left = (offset, size[y] - 1 - bot.legs.height - bot.body.height + 1)
+    top_right = (offset + bot.head.width - 1, size[y] - bot.legs.height - bot.body.height - bot.head.height + 1)
+    draw.rectangle([bot_left, top_right], fill=colors[2], outline=colors[0])
 
-
-
-    """ cleanup """
-
+    
     del draw
-    robot.show()
-    robot.save('robot.png')
-
-
-factory2(colors, size)
-
-#class robot:
-     
+    canvas.show()
+    canvas.save('robot.png')
+    
+    
+factory(colors, size)    
+    
+ 
+    
